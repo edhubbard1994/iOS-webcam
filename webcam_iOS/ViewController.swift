40876f6c0ca33f34e49be31dca926444affcbf5a
Ed Hubbard <Edhubbard1994@gmail.com>
2020-05-16 12:00:41 -0500

    Initial Commit

---
+++ webcam_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webcam_iOS
+//
+//  Created by Edward Hubbard on 5/16/20.
+//  Copyright © 2020 Edward Hubbard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ webcam_iOSTests/webcam_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  webcam_iOSTests.swift
+//  webcam_iOSTests
+//
+//  Created by Edward Hubbard on 5/16/20.
+//  Copyright © 2020 Edward Hubbard. All rights reserved.
+//
+
+import XCTest
+@testable import webcam_iOS
+
+class webcam_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
