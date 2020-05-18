2a11f51360fbcada47aeef267813ed1708c4e707
Ed Hubbard <Edhubbard1994@gmail.com>
2020-05-17 22:32:43 -0500

    initial

--- webcam_iOS/ViewController.swift
+++ webcam_iOS/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var session: AVCaptureSession?
+    var vidView: PreviewView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(AVCaptureDevice.authorizationStatus(for: .video).rawValue)
+        AVCaptureDevice.requestAccess(for: .video, completionHandler: { (didComplete) in
+            if (!didComplete) { return }
+            self.startCamera()
+        })
+        getVideoWindow()
+        self.session?.startRunning()
     }
+    
+    func getVideoWindow() -> Void {
+        self.vidView = PreviewView()
+        self.vidView?.videoPreviewLayer.session = self.session
+    }
+    
+    func startCamera() -> Bool {
+  
+        
+        self.session = AVCaptureSession()
+        
+        let device = AVCaptureDevice.default(.builtInDualCamera,for: .video, position: .front)
+        guard
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: device!) as! AVCaptureInput,
+            self.session!.canAddInput(videoDeviceInput)
+        else { return false }
+        self.session!.addInput(videoDeviceInput)
+        
+        let videoOutput = AVCaptureVideoDataOutput()
+        let soundOutput = AVCaptureAudioDataOutput()
+        guard self.session!.canAddOutput(videoOutput) else { return false }
+        guard self.session!.canAddOutput(soundOutput) else { return false }
+        self.session!.sessionPreset = .hd1280x720
+        self.session!.addOutput(videoOutput)
+        self.session!.addOutput(soundOutput)
+        self.session!.commitConfiguration()
+        
+        return true
+    }
+}
 
 
+class PreviewView: UIView {
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    /// Convenience wrapper to get layer as its statically known type.
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
 }
 
